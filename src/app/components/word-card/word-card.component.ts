db38c81d2c368f072b1c5423c1cdc8c066adb464
Hieu Le <ngochieu.le1708@gmail.com>
2023-08-14 23:51:27 +0700

    Add bulma css

--- src/app/components/word-card/word-card.component.ts
+++ src/app/components/word-card/word-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-word-card',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   standalone: true
 })
 export class WordCardComponent {
-
+  @Input() word: string;
+  @Input() number: number;
 }
